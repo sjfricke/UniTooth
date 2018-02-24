11e62577e939e1ffe470ca52ad12d0575bae898e
Brady Boettcher <bboettcher2@wisc.edu>
2018-02-24 17:45:48 -0500

    bluetooth connect and send data complete
    dll created

--- src/Unity/unitooth.cs
+++ src/Unity/unitooth.cs
@@ -5,89 +5,33 @@ using System.Runtime.InteropServices;
 
 public class unitooth : MonoBehaviour {
 
-    private static int AF_BTH = 32; //bluetooth address family
-    private static int SOCK_STREAM = 1; //socket type of TCP
-    private static int BTHPROTO_RFCOMM = 3; //RFCOMM protocol type
-    private static int INVALID_SOCKET = -1; //invalid socket identifier
-    private static int uniSocket;
-    public byte[] btaddr;
-
     public string macAddress;
     public int port;
 
-    public struct SOCKADDR_BTH
-    {
-        public ushort addressFamily;
-        public byte[] btAddr;
-        public System.Guid serviceClassId;
-        public ulong port;
-    };
-
 #if UNITY_STANDALONE_WIN || UNITY_EDITOR
-    [DllImport("ws2_32.dll", EntryPoint = "socket")]
-    public static extern int socket(int af, int type, int protocol);
-    [DllImport("ws2_32.dll", EntryPoint = "connect")]
-    public static extern int connect(int s, SOCKADDR_BTH name, int namelen);
-    [DllImport("ws2_32.dll", EntryPoint = "WSAGetLastError")]
-    public static extern int WSAGetLastError();
 
-    public static int socket()
-    {
-        return socket(AF_BTH, SOCK_STREAM, BTHPROTO_RFCOMM);
-    }
+    [DllImport("unitoothLib")]
+    public unsafe static extern int uniConnect(ulong mac_address);
 
-    public static int connect(int socketfd, byte[] btaddr, int port)
-    {
-        SOCKADDR_BTH newSocketAddr = new SOCKADDR_BTH();
-        newSocketAddr.addressFamily = (ushort)AF_BTH;
-        newSocketAddr.btAddr = btaddr;
-        newSocketAddr.port = (ulong)port;
-        return connect(socketfd, newSocketAddr, sizeof(ushort) + 6 + sizeof(ulong));
-
-    }
 #endif
 
     // Use this for initialization
     void Start () {
-        uniSocket = socket();
-        if (uniSocket == INVALID_SOCKET)
-        {
-            Debug.LogError("Unable to start bluetooth. Please make sure Bluetooth is enabled on your computer");
-        }
-
         if (macAddress != null)
         {
             macAddress = macAddress.Replace("-", ""); //get rid of any shit people put into the string
             macAddress = macAddress.Replace(":", "");
             macAddress = macAddress.Replace(".", "");
-
-            Debug.Log(macAddress);
             if (macAddress.Length != 12)
             {
                 Debug.LogError("Invalid MAC Address entered");
             }
-            string[] bytes = new string[6];
-            int j = 0;
-            for (int i = 0; i < 12; i += 2)
-            {
-                bytes[j] = macAddress.Substring(i, 2);
-                j++;
-            }
-            btaddr = new byte[6];
-            for (int i = 0; i < 6; i++)
-            {
-                btaddr[i] = byte.Parse(bytes[i], System.Globalization.NumberStyles.HexNumber);
-            }
-            int res = connect(uniSocket, btaddr, port);
+            ulong newVal = ulong.Parse(macAddress, System.Globalization.NumberStyles.HexNumber);
+            int res = uniConnect(newVal);
             Debug.Log("Result: " + res);
-            if (res == -1)
-            {
-                Debug.Log("Error: " + WSAGetLastError());
-            }
         } else {
             Debug.LogError("Please make sure to enter MAC Address of target device into script");
         }
-        
 	}
 	
 	// Update is called once per frame
