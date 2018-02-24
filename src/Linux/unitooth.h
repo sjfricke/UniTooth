ad165539b7a7094601daa49ecf661438047195f3
JoeSender <jsender@wisc.edu>
2018-02-24 16:28:05 -0600

    setting macros

--- src/Linux/unitooth.h
+++ src/Linux/unitooth.h
@@ -5,4 +5,7 @@ int run_listen(int argc, char **argv);
 int run_connect(int argc, char **argv);
 int run_find(int argc, char **argv);
 
+#define camera-pi	B8:27:EB:A5:10:E2
+#define zero-pi		B8:27:EB:56:69:24
+
 #endif
