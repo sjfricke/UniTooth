33fe93b6de11a07735e2484e24ddbf0bb59be58d
Vaughn Kottler <vkottler@wisc.edu>
2018-02-24 11:38:23 -0600

    sample implementation for testing master and slave

--- src/Python/unitooth/master.py
+++ src/Python/unitooth/master.py
@@ -3,3 +3,33 @@
 unitooth - master utilities
 """
 
+# built-in
+import bluetooth
+
+# package
+from . import SDP_NAME, SDP_UUID
+
+def listen(protocol):
+    """ server socket wrapper """
+
+    if protocol != bluetooth.RFCOMM and protocol != bluetooth.L2CAP:
+        print "protocol not valid."
+        return False
+
+    ssock = bluetooth.BluetoothSocket(protocol)
+    port = bluetooth.get_available_port(protocol)
+
+    bluetooth.advertise_service(ssock, SDP_NAME, SDP_UUID)
+
+    # blocks?
+    csock, addr = ssock.accept()
+
+    print "Accepted connection from ", addr
+
+    data = csock.recv(1024)
+    print "received [%s]" % data
+
+    csock.close()
+    ssock.close()
+
+    return True
--- src/Python/unitooth/slave.py
+++ src/Python/unitooth/slave.py
@@ -3,3 +3,31 @@
 unitooth - slave utilities
 """
 
+# built-in
+import bluetooth
+
+# package
+from . import SDP_NAME, SDP_UUID
+
+def connect(protocol):
+    """ client socket wrapper """
+
+    if protocol != bluetooth.RFCOMM and protocol != bluetooth.L2CAP:
+        print "protocol not valid."
+        return False
+
+    matches = bluetooth.find_service(uuid=SDP_UUID, name=SDP_NAME)
+
+    if not len(matches):
+        print "No %s services found." % SDP_NAME
+        return False
+
+    for match in matches:
+        print "'%s' %s:%s" % match["name"], match["host"], match["port"]
+
+    sock = bluetooth.BluetoothSocket(protocol)
+    sock.connect((match["host"], match["port"]))
+    sock.send("hello!!")
+    sock.close()
+
+    return True
