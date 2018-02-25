738ba733fe29388d3042f1b5bce52c15728b3fe5
sjfricke <spencerfricke@gmail.com>
2018-02-25 00:48:43 +0000

    working pi Light_switch minus bluetooth

--- examples/Light_Switch/Raspberry_Pi/light_switch.c
+++ examples/Light_Switch/Raspberry_Pi/light_switch.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <wiringPi.h>
 
@@ -6,26 +7,48 @@
 
 #define BUTTON 4
 #define LED 17
+#define POT_INTERVAL 103
+#define DEBOUNCE_THRESH 200
+
+static last_isr_time = 0;
+
+void buttonISR(void) {
+
+  unsigned long isr_time = millis();
+
+  if (isr_time - last_isr_time > DEBOUNCE_THRESH) {
+    usleep(100000); // ISR happens too fast when going 0 to 1
+    if (digitalRead(BUTTON)) {
+      puts("button on");
+    } else {
+      puts("button off");
+    }
+  }
+
+  last_isr_time = isr_time;
+}
 
 int main(int argc, char* argv[]) {
 
+  int pot_in;
   wiringPiSetupGpio();
 
+  // Setup button
   pinMode(BUTTON, INPUT);
-  pinMode(LED, OUTPUT);
   pullUpDnControl(BUTTON, PUD_UP);
+  wiringPiISR (BUTTON, INT_EDGE_BOTH, &buttonISR);
+
+  // Setup LED light
+  pinMode(LED, OUTPUT);
+
+  // Setup Analog input from ads1115
   ads1115Setup (MY_BASE, 0x48);
-  
+
   // Main loop
   while(1) {
-    if (digitalRead(BUTTON)) {
-      digitalWrite(LED, HIGH);
-    } else {
-      digitalWrite(LED, LOW);
-    }
 
-    int ch0 = analogRead (MY_BASE + 0) ;
-    printf("X: %d\n", ch0);
+    pot_in = analogRead(MY_BASE + 0)/POT_INTERVAL;
+    printf("X: %d\n", pot_in);
 
     usleep(100000);
   }
