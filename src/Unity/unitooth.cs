6ad230a8cf5ecbd13834ceb3a1c7fc38043499bc
Cale Geffre <geffre@wisc.edu>
2018-02-25 08:35:23 -0600

    Generalize unitooth
    Generalized unitooth script for any use

--- src/Unity/unitooth.cs
+++ src/Unity/unitooth.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
+using System.Threading;
 
-public class unitooth : MonoBehaviour {
+public class unitooth : MonoBehaviour
+{
 
     public string macAddress;
     public int port;
+    public int sendVal;
+    private int lastSentVal = -10;
 
 #if UNITY_STANDALONE_WIN || UNITY_EDITOR
 
@@ -14,11 +18,20 @@ public class unitooth : MonoBehaviour {
     public unsafe static extern int uniConnect(ulong mac_address);
     [DllImport("unitoothLib")]
     public static extern int receiveData();
-
+	[DllImport("unitoothLib")]
+	public static extern int sendData(int data);
 #endif
 
+    public int data;
+    bool _threadReceiveRunning;
+    bool _threadSendRunning;
+    Thread _threadReceive;
+    Thread _threadSendinIt;
+
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         if (macAddress != null)
         {
             macAddress = macAddress.Replace("-", ""); //get rid of any shit people put into the string
@@ -31,19 +44,82 @@ public class unitooth : MonoBehaviour {
             ulong newVal = ulong.Parse(macAddress, System.Globalization.NumberStyles.HexNumber);
             int res = uniConnect(newVal);
             Debug.Log("Result: " + res);
-        } else {
+        }
+        else
+        {
             Debug.LogError("Please make sure to enter MAC Address of target device into script");
         }
+        _threadReceive = new Thread(ThreadedWorkRec);
+        _threadReceive.Start();
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        int data = receiveData();
-        if (data != -1)
-        {
-            Debug.Log("Data: " + data);
-        }
-        
     }
+
+    void ThreadedWorkRec()
+    {
+        _threadReceiveRunning = true;
+        bool workDone = false;
+        while (_threadReceiveRunning && !workDone)
+        {
+            //whatevs
+            int data = receiveData();
+            if (data != -1)
+            {
+                Debug.Log("Data: " + data);
+            }
+            workDone = true;
+        }
+        _threadReceiveRunning = false;
+    }
+
+    void ThreadedWorkSend()
+    {
+        _threadSendRunning = true;
+        bool sentWorkDone = false;
+        while (_threadSendRunning && !sentWorkDone)
+        {
+            int returnVal = sendData(sendVal);
+            lastSentVal = sendVal;
+            sentWorkDone = true;
+        }
+        _threadSendRunning = false;
+    }
+
+
+
+    void OnDisable()
+    {
+        if (_threadReceiveRunning)
+        {
+            _threadReceiveRunning = false;
+            _threadReceive.Join();
+        }
+
+        if (_threadSendRunning)
+        {
+            _threadSendRunning = false;
+            _threadSendinIt.Join();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (!_threadReceiveRunning)
+        {
+            _threadReceive = new Thread(ThreadedWorkRec);
+            _threadReceive.Start();
+        }
+
+        // Send a value if it is non-null and hasn't yet been sent 
+        if (sendVal != null && !_threadSendRunning && lastSentVal != sendVal)
+        {
+            _threadSendinIt = new Thread(ThreadedWorkSend);
+            _threadSendinIt.Start();
+        }
+    }
+
+
 }
